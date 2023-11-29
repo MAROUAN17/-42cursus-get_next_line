dd9f0207fb03a29ff8400bce12256e691fbf3bc6
MAROUAN AGLAGAL <92761234+MAROUAN17@users.noreply.github.com>
2023-11-29 21:23:34 +0100

    fixing memory leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: maglagal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 18:42:42 by maglagal          #+#    #+#             */
-/*   Updated: 2023/11/29 11:21:21 by maglagal         ###   ########.fr       */
+/*   Updated: 2023/11/29 21:22:57 by maglagal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@
     # define BUFFER_SIZE 999
 # endif
 
-char    *freeing_memory(char *ptr)
+char    *freeing_memory(char **ptr)
 {
-    free(ptr);
-    ptr = NULL;
+    free(*ptr);
+    *ptr = NULL;
     return (NULL);
 }
 
-char    *make_line(char **buffer, char *line_rest, int *ptr_i)
+char    *make_line(char **buffer, char **next_buffer, int *ptr_i, char **temp)
 {
     char        *line;
     
@@ -35,79 +35,97 @@ char    *make_line(char **buffer, char *line_rest, int *ptr_i)
     line = ft_strdup(*buffer);
     if (!line)
     {
-        freeing_memory(line);
-        return (freeing_memory(*buffer));
+        freeing_memory(next_buffer);
+        return (freeing_memory(buffer));
     }
-    free(*buffer);
-    if (line_rest && line_rest[0] != '\0')
-    {    
-        *buffer = ft_strdup(line_rest);
+    freeing_memory(buffer);
+    if (*next_buffer && *next_buffer[0] != '\0')
+    {
+        *buffer = ft_strdup(*next_buffer);
+        freeing_memory(next_buffer);
         if (!*buffer)
-        {
-            freeing_memory(line);    
-            return (freeing_memory(*buffer));
-        }
+            return (freeing_memory(&line));
     }
-    else
-        *buffer = NULL;
+    freeing_memory(next_buffer);
+    freeing_memory(temp);
     return (line);
 }
 
 void    terminating_string(char *buffer)
 {
-    int i;
+    int     i;
+    char    *position;
 
     i = 0;
-    while (buffer[i] != '\n')
-        i++;
-    buffer[i + 1] = '\0';
+    position = ft_strchr(buffer, '\n');
+    position++;
+    *position = '\0';
 }
 
 char    *get_next_line(int fd)
 { 
     int                 nbytes;
     static char         *buffer;
-    char                temp[BUFFER_SIZE];
+    char                *temp;
     char                *temp2;
     char                *line_rest;
     char                *check_nl;
+    char                *n_buffer;
     static int          i;
-    
+
     line_rest = NULL;
+    n_buffer = NULL;
+    temp = NULL;
     if (fd < 0)
         return (NULL);
+    temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (!temp)
+        return (freeing_memory(&buffer));
     nbytes = read(fd, temp, BUFFER_SIZE);
     while (nbytes > 0 || (buffer && ft_strchr(buffer, '\n')))
     {
-        temp[nbytes] = '\0';
+        *(temp + nbytes) = '\0';
         i += nbytes;
         if (buffer == NULL)
         {
             buffer = ft_strdup(temp);
             if (!buffer)
-                return (freeing_memory(buffer));
+            {
+                freeing_memory(&temp);
+                return (freeing_memory(&buffer));
+            }
         }
         else
         {
             temp2 = buffer;
             buffer = ft_strjoin(temp2, temp);
             if (!buffer)
-                return (freeing_memory(buffer));
-            freeing_memory(temp2);
+            {
+                freeing_memory(&temp);   
+                return (freeing_memory(&buffer));
+            }
+            freeing_memory(&temp2);
         }
         if ((check_nl = ft_strchr(buffer, '\n')) != NULL)
         {
-            line_rest = ft_strdup(check_nl);
-            if (!line_rest)
-                return (freeing_memory(line_rest));
-            line_rest++;
+            check_nl++;
+            if (*check_nl != '\0')
+            {
+                line_rest = ft_strdup(check_nl);
+                if (!line_rest)
+                {
+                    freeing_memory(&temp);    
+                    return (freeing_memory(&buffer));
+                }
+            }
             terminating_string(buffer);
-            return (make_line(&buffer, line_rest, &i));
+            return (make_line(&buffer, &line_rest, &i, &temp));
         }
         nbytes = read(fd, temp, BUFFER_SIZE);
     }
+    freeing_memory(&temp);
     if (buffer && nbytes == 0)
-        return (make_line(&buffer, line_rest, &i));
+        return (make_line(&buffer, &line_rest, &i, &temp));
     return (NULL);
 }
 
@@ -117,6 +135,7 @@ char    *get_next_line(int fd)
 //     int fd;
 
 //     fd = open("test.txt", O_RDWR | O_CREAT);
+//     printf("line -> %s", get_next_line(fd));
 //     printf("line -> %s", get_next_line(fd));
 //     printf("line -> %s", get_next_line(fd));
 //     printf("line -> %s", get_next_line(fd));

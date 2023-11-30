d037fd77f85915feacb4e236e7f60ff57022fc96
MAROUAN AGLAGAL <92761234+MAROUAN17@users.noreply.github.com>
2023-11-30 11:44:21 +0100

    gnlTester memory leaks all true

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: maglagal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 18:42:42 by maglagal          #+#    #+#             */
-/*   Updated: 2023/11/29 21:43:02 by maglagal         ###   ########.fr       */
+/*   Updated: 2023/11/30 11:42:43 by maglagal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "get_next_line.h"
 
 # ifndef BUFFER_SIZE
@@ -27,7 +28,7 @@ char    *freeing_memory(char **ptr)
     return (NULL);
 }
 
-char    *make_line(char **buffer, char **next_buffer, int *ptr_i, char **temp)
+char    *make_line(char **buffer, char **next_buffer, int *ptr_i, char **reading_buffer)
 {
     char        *line;
     
@@ -35,7 +36,7 @@ char    *make_line(char **buffer, char **next_buffer, int *ptr_i, char **temp)
     line = ft_strdup(*buffer);
     if (!line)
     {
-        freeing_memory(temp);
+        freeing_memory(reading_buffer);
         freeing_memory(next_buffer);
         return (freeing_memory(buffer));
     }
@@ -43,94 +44,89 @@ char    *make_line(char **buffer, char **next_buffer, int *ptr_i, char **temp)
     if (*next_buffer && *next_buffer[0] != '\0')
     {
         *buffer = ft_strdup(*next_buffer);
-        freeing_memory(next_buffer);
         if (!*buffer)
         {
-            freeing_memory(buffer); //if buffer already has value allocated
-            freeing_memory(temp);
+            freeing_memory(reading_buffer);
             return (freeing_memory(&line));
         }
     }
     freeing_memory(next_buffer);
-    freeing_memory(temp);
+    freeing_memory(reading_buffer);
     return (line);
 }
 
-void    terminating_string(char *buffer)
+char    *making_buffer(char **buffer, char **reading_buffer)
 {
-    int     i;
-    char    *position;
+    char    *temp_buffer;
+    
+    if (*buffer == NULL)
+    {
+        *buffer = ft_strdup(*reading_buffer);
+        if (!*buffer)
+            freeing_memory(reading_buffer);
+    }
+    else
+    {
+        temp_buffer = *buffer;
+        *buffer = ft_strjoin(temp_buffer, *reading_buffer);
+        if (!*buffer)
+            freeing_memory(reading_buffer);
+        freeing_memory(&temp_buffer);
+    }
+    return (*buffer);
+}
 
-    i = 0;
-    position = ft_strchr(buffer, '\n');
-    position++;
-    *position = '\0';
+char    *check_newline(char **buffer, char **reading_buffer, char **line_rest)
+{
+    char    *check_nl;
+    
+    check_nl = ft_strchr(*buffer, '\n');
+    if (check_nl != NULL)
+    {
+        check_nl++;
+        if (*check_nl != '\0')
+        {
+            *line_rest = ft_strdup(check_nl);
+            if (!*line_rest)
+            {
+                freeing_memory(reading_buffer);
+                return (freeing_memory(buffer));
+            }
+        }
+        *check_nl = '\0';
+    }
+    return (check_nl);
 }
 
 char    *get_next_line(int fd)
 { 
     int                 nbytes;
     static char         *buffer;
-    char                *temp;
-    char                *temp2;
+    char                *reading_buffer;
     char                *line_rest;
     char                *check_nl;
-    char                *n_buffer;
     static int          i;
 
-    line_rest = NULL;
-    n_buffer = NULL;
-    temp = NULL;
     if (fd < 0)
         return (NULL);
-    temp = malloc(sizeof(char) * BUFFER_SIZE);
-    if (!temp)
+    line_rest = NULL;
+    reading_buffer = malloc(sizeof(char) * BUFFER_SIZE);
+    if (!reading_buffer)
         return (freeing_memory(&buffer));
-    nbytes = read(fd, temp, BUFFER_SIZE);
+    nbytes = read(fd, reading_buffer, BUFFER_SIZE);
     while (nbytes > 0 || (buffer && ft_strchr(buffer, '\n')))
     {
-        *(temp + nbytes) = '\0';
+        *(reading_buffer + nbytes) = '\0';
         i += nbytes;
-        if (buffer == NULL)
-        {
-            buffer = ft_strdup(temp);
-            if (!buffer)
-            {
-                freeing_memory(&temp);
-                return (freeing_memory(&buffer));
-            }
-        }
-        else
-        {
-            temp2 = buffer;
-            buffer = ft_strjoin(temp2, temp);
-            if (!buffer)
-            {
-                freeing_memory(&temp);   
-                return (freeing_memory(&buffer));
-            }
-            freeing_memory(&temp2);
-        }
-        if ((check_nl = ft_strchr(buffer, '\n')) != NULL)
-        {
-            check_nl++;
-            if (*check_nl != '\0')
-            {
-                line_rest = ft_strdup(check_nl);
-                if (!line_rest)
-                {
-                    freeing_memory(&temp);
-                    return (freeing_memory(&buffer));
-                }
-            }
-            terminating_string(buffer);
-            return (make_line(&buffer, &line_rest, &i, &temp));
-        }
-        nbytes = read(fd, temp, BUFFER_SIZE);
+        making_buffer(&buffer, &reading_buffer);
+        check_nl = check_newline(&buffer, &reading_buffer, &line_rest);
+        if (check_nl)
+            return (make_line(&buffer, &line_rest, &i, &reading_buffer));
+        nbytes = read(fd, reading_buffer, BUFFER_SIZE);
     }
-    freeing_memory(&temp);
+    freeing_memory(&reading_buffer);
     if (buffer && nbytes == 0)
-        return (make_line(&buffer, &line_rest, &i, &temp));
+        return (make_line(&buffer, &line_rest, &i, &reading_buffer));
     return (NULL);
 }
 
@@ -138,6 +134,10 @@ char    *get_next_line(int fd)
 // int main()
 // {
 //     int fd;
+//     clock_t start, end;
+//     double cpu_time_used;
+
+//     start = clock();
 
 //     fd = open("test.txt", O_RDWR | O_CREAT);
 //     printf("line -> %s", get_next_line(fd));
@@ -152,4 +152,7 @@ char    *get_next_line(int fd)
 //     printf("line -> %s", get_next_line(fd));
 //     printf("line -> %s", get_next_line(fd));
 //     printf("line -> %s", get_next_line(fd));
+//     end = clock();
+//     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+//     printf("\nExecution time: %f seconds\n", cpu_time_used);
 // }
